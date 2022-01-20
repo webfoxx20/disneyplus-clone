0b960e504731e83da3241c76ce9a64be2a60a2b2
Ayoola Oladipe <haywise20@gmail.com>
2022-01-21 00:10:51 +0100

    created a slide caurosel

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,9 @@
-// import SliderImg from "./SliderImg";
+import SliderImg from "./SliderImg";
 
 const Home = () => {
   return (
     <main className="container mx-auto   px-8  main-content w-full text-white bg-body-background  bg-cover bg-no-repeat bg-center overflow-hidden">
-      {/* <SliderImg /> */}
+      <SliderImg />
     </main>
   );
 };
